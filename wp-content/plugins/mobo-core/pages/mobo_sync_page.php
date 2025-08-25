1b4bc0a5efd59ba68216dacd38804901808d5f2e
Pedram Karimi <thepedramkarimi@gmail.com>
2025-08-26 00:38:24 +0330

    S

--- wp-content/plugins/mobo-core/pages/mobo_sync_page.php
+++ wp-content/plugins/mobo-core/pages/mobo_sync_page.php
@@ -6,15 +6,17 @@ if (!defined('ABSPATH')) {
 }
 // https://mobomobo.ir/admin/store/products/159500255
 
-function mobo_core_sync_categories(){
-        $apiFunc = new \MoboCore\ApiFunctions();
+function mobo_core_sync_categories()
+{
+    $apiFunc = new \MoboCore\ApiFunctions();
 
-        $categoriesDataJson = $apiFunc->getCategoriesAsJson();
-        $catFunc = new \MoboCore\WooCommerceCategoryManager();
-        $catFunc->addOrUpdateAllCategories($categoriesDataJson);
+    $categoriesDataJson = $apiFunc->getCategoriesAsJson();
+    $catFunc = new \MoboCore\WooCommerceCategoryManager();
+    $catFunc->addOrUpdateAllCategories($categoriesDataJson);
 }
 
-function mobo_core_sync_products() {
+function mobo_core_sync_products()
+{
     $apiFunc = new \MoboCore\ApiFunctions(); // Replace with your API function class
     $productFunc = new \MoboCore\WooCommerceProductManager(); // Replace with your product function class
 
@@ -26,8 +28,8 @@ function mobo_core_sync_products() {
     if (is_null($productLeft)) {
         // Initial setup
         $totalCount =  $apiFunc->getProductsCount();
-        if($totalCount === false){
-            add_action('admin_notices', function() {
+        if ($totalCount === false) {
+            add_action('admin_notices', function () {
                 echo '<div class="error"><p>خطا در دریافت تعداد محصولات</p></div>';
             });
             return;
@@ -36,17 +38,16 @@ function mobo_core_sync_products() {
 
         $productLeft = $totalCount;
         update_option('mobo_sync_product_left', $productLeft);
-    }
-    else{
+    } else {
         $productLeft = intval($productLeft);
     }
 
 
     // Process products
-    if($productLeft > 0) {
+    if ($productLeft > 0) {
         $productsDataJson = $apiFunc->getProductsAsJson($page, $pageSize);
-        if($productsDataJson === false){
-            add_action('admin_notices', function() {
+        if ($productsDataJson === false) {
+            add_action('admin_notices', function () {
                 echo '<div class="error"><p>خطا در دریافت محصولات</p></div>';
             });
             return;
@@ -74,55 +75,90 @@ add_action('mobo_core_sync_products_event', 'mobo_core_sync_products');
 add_action('mobo_core_sync_categories_event', 'mobo_core_sync_categories');
 
 // Admin page function
-function mobo_core_sync_page() {
+function mobo_core_sync_page()
+{
 
     $apiFunc = new \MoboCore\ApiFunctions(); // Replace with your API function class
     $info = $apiFunc->getLicenseInfo();
+    $page = intval(get_option('mobo_sync_page', 1));
+    $productLeft = get_option('mobo_sync_product_left', null);
 
-    if(!mobo_isLicenseExpired()){
-    if (isset($_POST['submit'])) {
-        // Verify nonce for security
-        check_admin_referer('mobo_core_sync_categories');
-        
-        // Optional: Add an admin notice
-        add_action('admin_notices', function() {
-            echo '<div class="updated"><p>همگام سازی دسته بندی موبو کور با موفقیت انجام شد!</p></div>';
-        });
+    if (!mobo_isLicenseExpired()) {
+        if (isset($_POST['submit'])) {
+            if (isset($_POST['mobo_core_sync_categories'])) {
+                // Verify nonce for security
+                check_admin_referer('mobo_core_sync_categories_nounce');
 
-        $apiFunc = new \MoboCore\ApiFunctions();
+                // Optional: Add an admin notice
+                add_action('admin_notices', function () {
+                    echo '<div class="updated"><p>همگام سازی دسته بندی موبو کور با موفقیت انجام شد!</p></div>';
+                });
 
-        $categoriesDataJson = $apiFunc->getCategoriesAsJson();
-        $catFunc = new \MoboCore\WooCommerceCategoryManager();
-        $catFunc->addOrUpdateAllCategories($categoriesDataJson);
+                $apiFunc = new \MoboCore\ApiFunctions();
 
-        if (!wp_next_scheduled('mobo_core_sync_products_event')) {
-            wp_schedule_event(time(), 'mobo_core_product_interval', 'mobo_core_sync_products_event');
+                $categoriesDataJson = $apiFunc->getCategoriesAsJson();
+                $catFunc = new \MoboCore\WooCommerceCategoryManager();
+                $catFunc->addOrUpdateAllCategories($categoriesDataJson);
+
+                if (!wp_next_scheduled('mobo_core_sync_products_event')) {
+                    wp_schedule_event(time(), 'mobo_core_product_interval', 'mobo_core_sync_products_event');
+                }
+
+
+                if (!wp_next_scheduled('mobo_core_sync_categories_event')) {
+                    wp_schedule_event(time(), 'mobo_core_categories_interval', 'mobo_core_sync_categories_event');
+                }
+            } else if (isset($_POST['mobo_core_update_setting'])) {
+                check_admin_referer('mobo_core_update_setting_nounce');
+                $page = $_POST['page'];
+                update_option('mobo_sync_page', $page);
+            }
         }
 
-        
-        if (!wp_next_scheduled('mobo_core_sync_categories_event')) {
-            wp_schedule_event(time(), 'mobo_core_categories_interval', 'mobo_core_sync_categories_event');
+        $pageLeft = ($productLeft % $page);
+        if($productLeft % $page != 0){
+            $pageLeft++;
         }
-    }
+
+?>
+        <p>
+            <?php echo $info['message']; ?>
+            <br />
+            <?php echo $info['timeLeft']; ?>
+        </p>
+        <p>
+            تعداد همگام سازی شده : <?php echo $page; ?> از <?php echo $pageLeft; ?>
+            <br />
+            زمان تقریبی پایان همگام سازی : <?php echo ($pageLeft * 30);  ?> ثانیه دیگر
+        </p>
+
+        <form method="post" action="">
+            <p>
+                در صورتی که نیاز دارید از اول همگام سازی صورت بگیرد شماره درخواست را به 1 تغییر دهید 
+                در غیر این صورت بهتر است به آن کاری نداشته باشید
+            </p>
+            <input type="hidden" name="mobo_core_update_setting" value="mobo_core_update_setting" />
+            <?php wp_nonce_field('mobo_core_update_setting_nounce'); ?>
+
+            <label for="page">شماره درخواست :</label>
+            <input type="number" name="page" id="page" step="1" value="<?php echo $page; ?>" />
+
+            <?php submit_button('بروزرسانی تنظیمات'); ?>
+        </form>
+
+        <form method="post" action="">
+            <input type="hidden" name="mobo_core_sync_categories" value="mobo_core_sync_categories" />
+            <?php wp_nonce_field('mobo_core_sync_categories_nounce'); ?>
+            <?php submit_button('همگام سازی'); ?>
+        </form>
+    <?php
+    } else {
     ?>
         <p>
             <?php echo $info['message']; ?>
             <br />
             <?php echo $info['timeLeft']; ?>
         </p>
-        <form method="post" action="">
-            <?php wp_nonce_field('mobo_core_sync_categories'); ?>
-            <?php submit_button('همگام سازی'); ?>
-        </form>
-    <?php
+<?php
     }
-else{
-    ?>
-    <p>
-        <?php echo $info['message']; ?>
-        <br />
-        <?php echo $info['timeLeft']; ?>
-    </p>
-    <?php
-}
 }
