58538780546318ab1a88e4409e14d57440a6595a
Pedram Karimi <thepedramkarimi@gmail.com>
2025-09-06 01:48:46 +0330

    s

--- wp-content/plugins/mobo-core/pages/mobo_products.php
+++ wp-content/plugins/mobo-core/pages/mobo_products.php
@@ -89,7 +89,7 @@ function render_mobo_products_page() {
     echo '<div style="margin-top: 10px;">';
     echo 'Show <select name="posts_per_page" id="posts_per_page">';
     
-    foreach ([5, 10, 20, 50] as $size) {
+    foreach ([5, 10, 20, 50,100,200,500,1000,1500,2000] as $size) {
         echo '<option value="' . $size . '"' . selected($posts_per_page, $size, false) . '>' . $size . '</option>';
     }
     
