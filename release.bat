dc72ed598023da86248293fc4a0304c4fad6d3e9
Pedram Karimi <thepedramkarimi@gmail.com>
2025-09-10 04:31:29 +0330

    3.8

---
+++ release.bat
@@ -0,0 +1,3 @@
+git tag 3.8
+git push origin 3.8
+gh release create v3.8 --title "Mobo Core 3.8" --notes "Add product date with Mobo!"
--- wp-content/plugins/mobo-core/mobo-core.php
+++ wp-content/plugins/mobo-core/mobo-core.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: mobo-core
 Description: بروزرسانی خودکار محصولات از https://mobomobo.ir/
-Version: 3.6
+Version: 3.8
 Author: Pedram Karimi
 Author URI: http://github.com/PedramDev/
 // Requires PHP: <=8.1.0
