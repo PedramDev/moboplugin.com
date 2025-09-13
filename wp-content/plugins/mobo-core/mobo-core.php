6b6a63d538636da836df7ef733f89635572fa296
Pedram Karimi <thepedramkarimi@gmail.com>
2025-09-13 18:55:19 +0330

    4.2

--- wp-content/plugins/mobo-core/mobo-core.php
+++ wp-content/plugins/mobo-core/mobo-core.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: mobo-core
 Description: بروزرسانی خودکار محصولات از https://mobomobo.ir/
-Version: 4.1
+Version: 4.2
 Author: Pedram Karimi
 Author URI: http://github.com/PedramDev/
 // Requires PHP: <=8.1.0
