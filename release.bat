a7cbe6a9fe2d0bbb3c14e4869a92496435dfc68c
Pedram Karimi <thepedramkarimi@gmail.com>
2025-09-13 13:40:55 +0330

    s

--- release.bat
+++ release.bat
@@ -1,3 +1,3 @@
-git tag 4.0
-git push origin 4.0
-gh release create v4.0 --title "Mobo Core 4.0" --notes "4.0"
+git tag 4.1
+git push origin 4.1
+gh release create v4.0 --title "Mobo Core 4.1" --notes "4.1"
